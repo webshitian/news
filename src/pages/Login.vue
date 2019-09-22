f6d3c9ea41d5d81a1eadd6966942aebdd01c5d06
webshitian <1047221702@qq.com>
2019-09-22 18:59:24 +0800

    04-添加输入框的状态

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -11,16 +11,21 @@
       </div>
 
       <!--输入框组件-->
-      <AuthInput
-      placeholder="手机号码"
-      
-      :value='form.username'
-      @input="handleUsername"
-      >
-      </AuthInput>  
-      
-      <!--登录按钮-->
-      <button @click="handleSubmit">登录按钮</button>
+      <div class="inputs">
+        <AuthInput
+        placeholder="手机号码"
+        
+        :value='form.username'
+        @input="handleUsername"
+
+        :rule="/^1[0-9]{4,10}$/"
+        err_message="手机号码格式不正确"
+        >
+        </AuthInput>  
+        
+        <!--登录按钮-->
+        <button @click="handleSubmit">登录按钮</button>
+      </div>
   </div>
 </template>
 
@@ -35,7 +40,7 @@ export default {
         form:{
           username:"",
           password:""
-        }
+        },
       }
     },
     //注册组件
@@ -80,6 +85,12 @@ export default {
     font-size: 126/360*100vw;
     color: #d81e06;
   }
+}
+
+.inputs{
+    input{
+      margin-bottom: 20px;
+    }
 }
 
 </style>
