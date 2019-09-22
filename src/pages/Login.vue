ddd1cea70ff4cb16d6cd50a452a33bd8c319dcc0
webshitian <1047221702@qq.com>
2019-09-22 16:25:52 +0800

    03-输入框双向数据绑定

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,58 +1,85 @@
 <template>
-  <div>
-      <!--关闭的按钮-->
-      <div class="close">
-          <span class="iconfont iconicon-test">
-
-          </span>
-      </div>
+    <div class="container">
+        <!--关闭的按钮-->
+        <div class="close">
+          <span class="iconfont iconicon-test"></span>
+        </div>  
       
       <!--logo-->
       <div class="logo">
           <span class="iconfont iconnew"></span>
       </div>
 
-      <!--用户密码输入框-->
-      <div>
-        <input type="text">  
-      </div>
-
-      <div>
-        <input type="text">  
-      </div>
+      <!--输入框组件-->
+      <AuthInput
+      placeholder="手机号码"
+      
+      :value='form.username'
+      @input="handleUsername"
+      >
+      </AuthInput>  
       
       <!--登录按钮-->
-      <button></button>
+      <button @click="handleSubmit">登录按钮</button>
   </div>
 </template>
 
 <script>
+//导入输入框组件
+import AuthInput from "@/components/AuthInput";
+
 export default {
     data(){
       return{
-        list:[]
+        //发送给后台的数据
+        form:{
+          username:"",
+          password:""
+        }
       }
+    },
+    //注册组件
+  components:{
+    AuthInput
+  },
+
+  methods:{
+    //传递给输入框组件，用于获取用户名
+    handleUsername(value){
+      this.form.username = value;
+    },
+    //表单提交
+    handleSubmit(){
+      console.log(this.form)
     }
+  }
 }
+
 </script>
 
-<style lang="less">
+<style scoped lang="less">
+    // scoped 作用域样式
     //lang声明样式的类型
-    .close{
-      padding:20px;
-      span{
-        font-size: 27/360 * 100vw;
-      }
-    }
 
-    .logo{
-      display:flex;
-      justify-content:center;
-      
-      span{
-        display:block;
-        font-size:126/360*100vw;
-        color: #d81e06;
-      }
-    }
+.container {
+  padding: 20px;
+}
+
+.close {
+  span {
+    font-size: 27/360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126/360*100vw;
+    color: #d81e06;
+  }
+}
+
 </style>
