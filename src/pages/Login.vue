211e545dac7c3f338dd735efb2c752bb62fcb3bb
webshitian <1047221702@qq.com>
2019-09-22 21:14:39 +0800

    08-添加基准路径和处理登录成功

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -70,11 +70,16 @@ export default {
       console.log(this.form)
 
       this.$axios({  
-        url:"http://localhost:3000/login",
+        url:"/login",
         method:"POST",//method相当于type
         data:this.form 
       }).then( res => { //.then的回调函数相当于success
-        console.log(res)
+        const{message} = res.data;
+        
+        if(message === "登录成功"){
+          //跳转到首页
+          this.$router.push("/")
+        }
       });
     }
   }
