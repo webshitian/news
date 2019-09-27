2bfbdb7adfbf083298a1e7127840fc2df8f1afde
webshitian <1047221702@qq.com>
2019-09-27 11:46:40 +0800

    35-跟帖列表的渲染

--- src/pages/UserComment.vue
+++ src/pages/UserComment.vue
@@ -3,32 +3,38 @@
       <HeaderNormal title="我的跟帖"/>
 
       <!--评论的列表-->
-      <div class="comment-item">
+      <div 
+      class="comment-item"
+      v-for="(item,index) in list"
+      :key="index"
+      >
           <div class="time">2019年9月27日10:53:24</div>
           
           <!--当前的评论回复的评论-->
-          <div class="parent">
+          <div class="parent" v-if="item.parent">
               <div class="parent-title">
-                  @:web小势天
+                  @:{{item.parent.user.nickname}}
               </div>
               <div class="parent-content">
-                  今天有没有打算撸代码？
+                  {{item.parent.content}}
               </div>
           </div>
-      </div>
+      
 
-      <!--个人评论的内容-->
-      <div class="conten">
-          现在还不知道呢！
-      </div>
+          <!--个人评论的内容-->
+          <div class="content">
+            {{item.content}} 
+          </div>
 
      
           <router-link to="#" class="article-link">
             <p>  
-              原文:今天有没有打算撸代码，如果不撸代码，你怎么能进BAT呢！30岁之前能进BAT就好咯！想想都开心
+              原文:{{item.post.title}}
             </p>
             <span class="iconfont">&#xe742;</span>  
           </router-link>
+
+     </div>
      
   </div>
 </template>
@@ -37,8 +43,29 @@
 //导入头部
 import HeaderNormal from "@/components/HeaderNormal";
 export default {
+    data(){
+        return{
+            list:[]
+        }
+    },
     components:{
         HeaderNormal
+    },
+    
+    mounted(){
+        //请求评论列表
+        this.$axios({
+            url:"/user_comments",
+            //添加头信息
+            headers:{
+                Authorization: localStorage.getItem("token")
+            }
+        }).then(res => {
+            const {data} = res.data;
+
+            //保存到data中
+            this.list = data;
+        })
     }
 }
 </script>
