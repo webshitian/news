4470601346e5ec6774ed6879047e0bd500afc8a7
webshitian <1047221702@qq.com>
2019-10-01 16:44:59 +0800

    56-根据id关注文章作者

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -7,7 +7,7 @@
                     <span class="iconfont">&#xe630;</span>
                     <span class="iconfont icon-xinlang"></span>
               </div>
-              <span class="focus" v-if="!detail.has_follow">关注</span>
+              <span class="focus" v-if="!detail.has_follow" @click="handleFollow">关注</span>
               <span class="focus focus_active" v-else>已关注</span>
           </div>
 
@@ -51,14 +51,46 @@ export default {
     components:{
         PostFooter
     },
+    methods:{
+        //关注当前的作者
+        handleFollow(){
+            //通过作者的id关注用户
+            this.$axios({
+                url:"/user_follows/" + this.detail.user.ud,
+                //添加头信息
+                headers: {
+                    Authorization: localStorage.getItem("token")
+                }
+            }).then(res => {
+                const {message} = res.data;
+
+                if(message === '关注成功'){
+                    //修改关注的按钮的状态
+                    this.detail.has_follow = true;
+                    this.$toast.success(message)
+                }
+            })
+        }
+    },
     mounted(){
         //请求文章的详情
         const {id} = this.$route.params;
 
-        this.$axios({
+        //token
+        const token = localStorage.getItem("token");
+        //请求的配置
+        const config = {
             url:"/post/" + id
-            
-        }).then(res => {
+        }
+
+        //如果有token就带上，才可能获取到是否关注，是否收藏的属性
+        if(token){
+            config.headers = {
+                Authorization:token
+            }
+        }
+
+        this.$axios(config).then(res => {
             const {data} = res.data;
             
             //保存到详情
